d59463bd0bddca9e3f57406df5d51472fb1c1683
SuperBesse <julien.besse@gsoi.fr>
2020-05-18 20:06:12 +0200

    Initial Commit

---
+++ Youtuber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Youtuber
+//
+//  Created by julien besse on 18/05/2020.
+//  Copyright © 2020 julien besse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
