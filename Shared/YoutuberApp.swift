cda98317b21d1e04e13945eac63fab1f021f921e
Gilles Bordas <gbordas@beonapps.com>
2020-06-29 13:47:49 +0200

    Initial Commit

---
+++ Shared/YoutuberApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutuberApp.swift
+//  Shared
+//
+//  Created by Gilles Bordas on 29/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct YoutuberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
