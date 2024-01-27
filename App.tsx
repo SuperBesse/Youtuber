1396857761484dc920965fb4146f488955b7364b
Julien Besse <julienbesse@amplement.com>
2024-01-27 18:35:04 +0100

    [setup]: create procject, configure youtube signin

--- App.tsx
+++ App.tsx
@@ -5,61 +5,70 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
-
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import auth from '@react-native-firebase/auth';
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      GoogleSignin.configure({
+        webClientId:
+          '8377471577-8iua8f5644ihni29k8nv0e8b0u7i4hhj.apps.googleusercontent.com',
+        scopes: ['https://www.googleapis.com/auth/youtube.readonly'],
+        offlineAccess: false,
+        forceCodeForRefreshToken: true,
+      });
+      await GoogleSignin.hasPlayServices();
+      await GoogleSignin.addScopes({
+        scopes: ['https://www.googleapis.com/auth/youtube.readonly'],
+      });
+
+      const {idToken} = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      return auth().signInWithCredential(googleCredential);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchYouTubeData = async () => {
+    const accessToken = await GoogleSignin.getTokens();
+
+    console.log('accessToken: ', accessToken);
+    const response = await fetch(
+      'https://www.googleapis.com/youtube/v3/channels?part=snippet&mine=true',
+      {
+        headers: {Authorization: `Bearer ${accessToken.accessToken}`},
+      },
+    );
+    const data = await response.json();
+    // Traitez les données reçues de l'API YouTube
+    console.log('DATA: ', data);
+  };
+  const onLoginTouch = () => {
+    signInWithGoogle();
   };
 
   return (
@@ -70,26 +79,24 @@ function App(): React.JSX.Element {
       />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+        contentContainerStyle={{flex: 1}}>
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flex: 1,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <GoogleSigninButton
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={onLoginTouch}
+            disabled={false}
+          />
+          <TouchableOpacity onPress={fetchYouTubeData}>
+            <Text style={{color: isDarkMode ? Colors.white : Colors.black}}>
+              FETCH
+            </Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
